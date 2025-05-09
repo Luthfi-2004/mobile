fca976685acee1f91cda8b79adc84a9572073cc9
Luthfi Rafananda Naufal <if23.luthfinaufal@mhs.ubpkarawang.ac.id>
2025-05-09 15:19:43 +0700

    push

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
-  standalone: false,
+  standalone: false
 })
-export class CartPage implements OnInit {
+export class CartPage {
+  cart: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    if (nav?.extras?.state && nav.extras.state['cart']) {
+      this.cart = nav.extras.state['cart'];
+    }
   }
 
+  getTotalHarga() {
+    return this.cart.reduce((total, item) => total + item.harga * item.quantity, 0);
+  }
+
+  checkout() {
+    // Logika submit reservasi ke backend bisa di sini
+    alert('Pesanan Anda berhasil dikirim!');
+    this.router.navigate(['/menu']);
+  }
+
+  goBack() {
+    this.router.navigate(['/menu']);
+  }
 }
