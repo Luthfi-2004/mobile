6820e8387c1bf3563290970fb99218c88a29a6bf
Alif IkrarPrabu <if23.alifprabu@mhs.ubpkarawang.ac.id>
2025-06-01 20:16:32 +0700

    login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { AuthService } from '../auth.service'; // Import AuthService
 
 @Component({
   selector: 'app-home',
@@ -8,31 +9,39 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
   images: { src: string; type: 'indoor' | 'outdoor' | 'vvip'; text?: string }[] = [
-  { 
-    src: 'assets/img/indoor.jpg', 
-    type: 'indoor',
-    text: 'Area Indoor' 
-  },
-  { 
-    src: 'assets/img/outdoor.jpg', 
-    type: 'outdoor',
-    text: 'Area Outdoor' 
-  },
-  { 
-    src: 'assets/img/vvip.jpg', 
-    type: 'vvip',
-    text: 'Area VVIP' 
-  }
-];
+    { 
+      src: 'assets/img/indoor.jpg', 
+      type: 'indoor',
+      text: 'Area Indoor' 
+    },
+    { 
+      src: 'assets/img/outdoor.jpg', 
+      type: 'outdoor',
+      text: 'Area Outdoor' 
+    },
+    { 
+      src: 'assets/img/vvip.jpg', 
+      type: 'vvip',
+      text: 'Area VVIP' 
+    }
+  ];
   currentImageIndex = 0;
 
   constructor(
     private router: Router,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private authService: AuthService // Tambahkan AuthService
   ) {
     this.startImageSlider();
+  }
+
+  ngOnInit() {
+    // Jika user belum login, arahkan ke halaman login
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    }
   }
 
   startImageSlider() {
@@ -50,7 +59,6 @@ export class HomePage {
     const riwayat = JSON.parse(localStorage.getItem('riwayat') || '[]');
 
     if (riwayat.length === 0) {
-      // Jika belum ada transaksi, beri pemberitahuan
       const alert = await this.alertController.create({
         header: 'Belum Ada Invoice',
         message: 'Anda belum melakukan pemesanan atau transaksi apapun.',
@@ -58,7 +66,6 @@ export class HomePage {
       });
       await alert.present();
     } else {
-      // Jika ada transaksi, arahkan ke halaman invoice
       const transaksiTerakhir = riwayat[riwayat.length - 1];
       this.router.navigate(['/invoice'], { state: { transaksi: transaksiTerakhir } });
     }
