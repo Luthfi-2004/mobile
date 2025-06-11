9041a11c710064d08182ad2239815a409c9ede4e
Reinaldi99 <if23.reinaldisukri@mhs.ubpkarawang.ac.id.com>
2025-06-11 17:14:36 +0700

    taiti

--- src/app/invoice-detail/invoice-detail.module.ts
+++ src/app/invoice-detail/invoice-detail.module.ts
@@ -1,19 +1,21 @@
+// invoice-detail.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { InvoiceDetailPageRoutingModule } from './invoice-detail-routing.module';
-
 import { InvoiceDetailPage } from './invoice-detail.page';
+// Import standalone component
+import { QRCodeComponent } from 'angularx-qrcode';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InvoiceDetailPageRoutingModule
+    InvoiceDetailPageRoutingModule,
+    QRCodeComponent  // ✅ benar
   ],
   declarations: [InvoiceDetailPage]
 })
